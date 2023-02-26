d1f6f485721a7703c6de3567cd0d49f013d2d130
humashaikhc <fatma_shaikh21@yahoo.com>
2023-02-26 20:52:15 +0000

    gcp bit done

--- db/index.js
+++ db/index.js
@@ -1,5 +1,9 @@
+import {config} from "dotenv";
 import { Sequelize } from "sequelize";
-export const sequelize = new Sequelize("chocolate_db", "root", "password", {
+
+config();
+
+export const sequelize = new Sequelize("chocolate_db", process.env.DB_USER, process.env.DB_PW, {
     dialect: "mysql",
-    host: "localhost",
+    host: process.env.DB_HOST,
   });
